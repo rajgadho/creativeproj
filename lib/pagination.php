f590b00dc47ff3cbe26ed7fad2dfb114008be346
rajgadho <raj.gadho@gmail.com>
2017-10-02 19:54:10 +0530

    d
    d

--- lib/pagination.php
+++
@@ -1,9 +0,0 @@
-<div class="teo-pagination">
-	<?php
-	if(function_exists('wp_pagenavi')) 
-		wp_pagenavi(); 
-	else { 
-		the_posts_pagination( array( 'mid_size' => 2, 'screen_reader_text' => ' ' ) );
-	}
-	?>
-</div>
